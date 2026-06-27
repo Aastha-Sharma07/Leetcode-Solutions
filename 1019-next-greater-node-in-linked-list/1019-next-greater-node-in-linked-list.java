a9cf5466cdda74cd340fe5d8dcefa482e9f34381
Aastha-Sharma07 <aasthaa.sh07@gmail.com>
2026-06-27 18:50:08 +0530

    Time: 21 ms (80.88%), Space: 50.2 MB (27.85%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
@@ -8,10 +8,10 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+ import java.util.*;
 class Solution {
     public int[] nextLargerNodes(ListNode head) {
         
-        // Step 1: Find size
         int size = 0;
         ListNode temp = head;
         while (temp != null) {
@@ -20,26 +20,28 @@ class Solution {
         }
 
         int[] answer = new int[size];
+        int[] arr = new int[size];
+        
+        temp = head;
+        int j = 0;
+        while(j < size && temp != null){
+            arr[j] = temp.val;
+            temp = temp.next;
+            j++;
+        }
 
-        // Step 2: For each node, find next greater
-        ListNode curr = head;
-        int i = 0;
+        Stack<Integer> s = new Stack<> ();
 
-        while (curr != null) {
-            ListNode next = curr.next;
-
-            while (next != null && next.val <= curr.val) {
-                next = next.next;
+        for(int i = size-1; i>=0 ; i--){
+            while(!s.isEmpty() && arr[s.peek()]<=arr[i]){
+                s.pop();
             }
-
-            if (next != null) {
-                answer[i] = next.val;
-            } else {
+            if(s.isEmpty()){
                 answer[i] = 0;
+            }else{
+                answer[i] = arr[s.peek()];
             }
-
-            curr = curr.next;
-            i++;
+            s.push(i);
         }
 
         return answer;
