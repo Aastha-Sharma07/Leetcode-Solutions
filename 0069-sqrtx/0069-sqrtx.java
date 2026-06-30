3a43fae3286e48caa7208de3add8cb09eebeb82d
Aastha-Sharma07 <aasthaa.sh07@gmail.com>
2026-06-30 22:58:43 +0530

    Time: 1 ms (97.73%), Space: 42.7 MB (22.97%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x == 0 || x == 1) return x;
+
+        int low = 1, high = x;
+        int ans = 0;
+
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+
+            if(mid <= x / mid){   // mid*mid <= x
+                ans = mid;
+                low = mid + 1;    // try bigger
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
